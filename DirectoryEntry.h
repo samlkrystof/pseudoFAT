ddbad537e385bdf01463712da1d033336cfe0c5e
samlkrystof <ksaml@gapps.zcu.cz>
2023-02-07 23:32:47 +0100

    FATTable and DirEntry definitions added

---
+++ DirectoryEntry.h
@@ -0,0 +1,14 @@
+//
+// Created by Kryst on 07.02.2023.
+//
+
+#ifndef PSEUDOFAT_DIRECTORYENTRY_H
+#define PSEUDOFAT_DIRECTORYENTRY_H
+
+#endif //PSEUDOFAT_DIRECTORYENTRY_H
+
+typedef struct {
+    char *name;
+    unsigned int size;
+    unsigned int firstCluster;
+} DirectoryEntry;
---
+++ FATTable.h
@@ -0,0 +1,14 @@
+//
+// Created by Kryst on 07.02.2023.
+//
+
+#ifndef PSEUDOFAT_FATTABLE_H
+#define PSEUDOFAT_FATTABLE_H
+
+#endif //PSEUDOFAT_FATTABLE_H
+
+typedef struct {
+    unsigned int entriesCount;
+    unsigned int freeEntriesCount;
+    unsigned int *entries;
+} FATTable;
